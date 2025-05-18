b35ab73c5a0e35fef9268232b8d32519f3fc9a89
n0access <95961325+n0access@users.noreply.github.com>
2025-05-18 18:48:28 +0100

    Delete pos directory

--- pos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('add-product/', views.add_product, name='add_product'),
-    path('products/', views.product_list, name='product_list'),
-]
